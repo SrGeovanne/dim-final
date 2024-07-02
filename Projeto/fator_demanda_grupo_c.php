648a84177b55a31d3d359c4b6adfd092ae7147cf
George Geovanne Silva Teixeira <67590912+SrGeovanne@users.noreply.github.com>
2024-07-02 00:52:05 -0300

    Delete Projeto directory

--- Projeto/fator_demanda_grupo_c.php
+++
@@ -1,7 +0,0 @@
-<?php
-function calcularFatorDemandaGrupoC($numAparelhos)
-{
-    $fatores = [1.00,1.00, 0.88, 0.82, 0.78, 0.76, 0.74, 0.72, 0.71, 0.70];
-    return $numAparelhos <= 10 ? $fatores[$numAparelhos - 1] : 0.70;
-}
-?>
